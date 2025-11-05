4378135d21af3280e4375f275863d4c4ae003a99
Mihai Bors <moreliciouscod@gmail.com>
2025-11-05 11:28:45 +0100

    no need

--- format.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Format with black
-black DAWPY/
-
-REM Lint & sort imports with ruff
-ruff check DAWPY/ --fix --unsafe-fixes
--- format.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Format with black
-black DAWPY/
-
-# Lint & sort imports with ruff
-ruff check DAWPY/ --fix --unsafe-fixes
